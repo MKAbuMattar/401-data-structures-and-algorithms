eaa22c3f74f246f12de53c05e6c5e306706f6e30
MKAbuMattar <mohammad.khaled@outlook.com>
2021-08-08 19:23:57 +0300

    ok

---
+++ trees/app/src/main/java/trees/data/KNode.java
@@ -0,0 +1,58 @@
+package trees.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KNode<T>{
+    private T data;
+    private List<KNode<T>> children;
+    private KNode<T> parent;
+
+
+    public KNode() {
+        children = new ArrayList<KNode<T>>();
+    }
+
+    public KNode(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public KNode<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(KNode<T> parent) {
+        this.parent = parent;
+    }
+
+
+    public List<KNode<T>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<KNode<T>> children) {
+        for(KNode<T> child : children) {
+            child.parent = this;
+        }
+
+        this.children = children;
+    }
+
+    public boolean hasChildren() {
+        return getChildren().size() > 0;
+    }
+
+    public void addChild(KNode<T> child) {
+        child.parent = this;
+        children.add(child);
+    }
+
+}
---
+++ trees/app/src/main/java/trees/types/KAryTree.java
@@ -0,0 +1,38 @@
+package trees.types;
+
+import trees.data.KNode;
+
+public class KAryTree<T> {
+    KNode<T> root = new KNode<>();
+
+    public KAryTree() {
+    }
+
+    public KNode<T> getRoot() {
+        return this.root;
+    }
+
+    public void setRoot(KNode<T> root) {
+        this.root = root;
+    }
+
+    public int getNumberOfNodes() {
+        int numberOfNodes = 0;
+
+        if(root != null) {
+            numberOfNodes = getNumberOfNodes(root) + 1;
+        }
+
+        return numberOfNodes;
+    }
+
+    private int getNumberOfNodes(KNode<T> node) {
+        int k = node.getChildren().size(); // k = number of nodes
+
+        for(KNode<T> child : node.getChildren()) {
+            k += getNumberOfNodes(child);
+        }
+
+        return k;
+    }
+}
