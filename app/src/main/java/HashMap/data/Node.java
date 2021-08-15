ce23b8d84fe350a42680bc8079b5751a9b607a8c
MKAbuMattar <mohammad.khaled@outlook.com>
2021-08-15 19:36:26 +0300

    hotfix

--- app/src/main/java/HashMap/data/Node.java
+++ app/src/main/java/HashMap/data/Node.java
@@ -3,6 +3,13 @@ package HashMap.data;
 public class Node<K, V> {
   private final K key;
   private final V value;
+  Node<K, V> next;
+
+  public Node(K key, V value, Node<K, V>  next) {
+    this.key = key;
+    this.value = value;
+    this.next = next;
+  }
 
   public Node(K key, V value) {
     this.key = key;
